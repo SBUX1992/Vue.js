e4acebc9e3be24db098e127a160f94409327aaae
sbux <sinsunkangg@naver.com>
2023-04-26 00:30:02 +0900

    aa

--- Voard/src/components/Write.vue
+++ Voard/src/components/Write.vue
@@ -10,45 +10,31 @@
     <v-main>
       <v-container>
         <v-sheet max-width="800" class="mx-auto mt-10" border>
-          <v-table>
-            <tbody>
-              <tr>
-                <th>제목</th>
-                <td>
-                  <v-text-field
-                    class="mt-10"
-                    variant="outlined"
-                    placeholder="제목"
-                  ></v-text-field>
-                </td>
-              </tr>
-
-              <tr>
-                <th>내용</th>
-                <td>
-                  <v-text-field
-                    class="mt-10"
-                    variant="outlined"
-                    hide-details="true"
-                    placeholder="내용을 입력해 주세요"
-                  ></v-text-field>
-                </td>
-              </tr>
-              <v-row>
-                <v-col cols="12">
-                  <v-text-field
-                    class="mt-10"
-                    variant="outlined"
-                    hide-details="true"
-                    placeholder="댓글을 입력해 주세요"
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="6">
-                  <v-btn color="success">등록</v-btn>
-                </v-col>
-              </v-row>
-            </tbody>
-          </v-table>
+          <v-row justify="center" align="center">
+            <v-col cols="12" sm="8" md="6">
+              <v-form>
+                <v-card class="mx-auto mt-6">
+                  <v-card-title>새 글쓰기</v-card-title>
+                  <v-card-text>
+                    <v-text-field
+                      variant="outlined"
+                      label="제목"
+                      type="text"
+                      placeholder="제목"
+                      required
+                    ></v-text-field>
+                    <v-textarea
+                      variant="outlined"
+                      label="내용"
+                      type="text"
+                      placeholder="내용"
+                      required
+                    ></v-textarea>
+                  </v-card-text>
+                </v-card>
+              </v-form>
+            </v-col>
+          </v-row>
           <v-sheet class="text-center pt-6">
             <v-btn @click="btnCancel">취소</v-btn>
             <v-btn class="ml-2" color="primary" @click="btnWrite">등록</v-btn>
